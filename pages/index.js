5c7b83d7a86f7a382018e1a6d682fbf1842ac191
Grinchenko Sergei <butters.blaga@gmail.com>
2022-05-07 23:57:39 +0900

    feat: added ability to edit name & information about yourself

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,47 @@
 const openButton = document.querySelector('.profile__button_action_edit');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.form__button_action_close');
+const saveButton = document.querySelector('.form__button_action_save');
 const togglePopup = function (event) {
   popup.classList.toggle("popup_opened");
 }
 
-openButton.addEventListener('click', togglePopup)
+// Закрываем и открываем попап кликом на соответсвующие кнопки
+openButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
+saveButton.addEventListener('click', togglePopup);
 
-popup.addEventListener('click', function (event) {
-  if (event.target === event.currentTarget)
-      togglePopup();
-})
 
+// Закрываем попап через клавишу Esc
 document.addEventListener('keyup', (ev) => {
   if (ev.key === "Escape" && popup.classList.contains("popup_opened")) {
-      togglePopup();
+    togglePopup();
   }
 })
 
+// Находим форму в DOM
+const formElement = document.querySelector('.form');
+
+// Находим поля формы в DOM
+const nameInput = formElement.querySelector('.form__item_type_name');
+const workInput = formElement.querySelector('.form__item_type_work');
+
+// Выбераем элементы, куда должны быть вставлены значения полей
+const profileName = document.querySelector('.profile__name');
+const profileWork = document.querySelector('.profile__work');
+
+// Получаем значение полей workInput и nameInput из свойства value
+nameInput.value = profileName.textContent;
+workInput.value = profileWork.textContent;
+
+// Обработчик «отправки» формы
+function formSubmitHandler (evt) {
+  evt.preventDefault();  // Отменяем стандартную отправку формы
+
+// Вставляем новые значения с помощью textContent
+  profileName.textContent = nameInput.value;
+  profileWork.textContent = workInput.value;
+}
+
+// Прикрепляем обработчик к форме
+formElement.addEventListener('submit', formSubmitHandler);
