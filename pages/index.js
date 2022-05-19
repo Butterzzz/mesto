064e66dc61a269b7a43ae7972acbf97f7ae8d268
Grinchenko Sergei <butters.blaga@gmail.com>
2022-05-20 03:42:49 +0900

    feat: two popups

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.profile__button_action_edit');
-const closeButton = document.querySelector('.popup__button_action_close');
+const popupProfileEdit = document.querySelector('.popup_type_edit-profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
+
+const editProfileButton = document.querySelector('.profile__button_action_edit');
+const addCardButton = document.querySelector('.profile__button_action_add');
+const closeButtons = document.querySelectorAll('.popup__button_action_close');
+
 const profileName = document.querySelector('.profile__name');
 const profileWork = document.querySelector('.profile__work');
 
@@ -8,7 +12,6 @@ const formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('.form__item_type_name');
 let workInput = formElement.querySelector('.form__item_type_work');
 
-//1. Шесть карточек «из коробки»
 const cardsList = document.querySelector('.cards__list');
 const cardTemplate = document.querySelector('.card-template').content;
 const initialCards = [
@@ -46,32 +49,47 @@ const initialCards = [
 
 initialCards.forEach((element) => {
   const cardElement = cardTemplate.cloneNode(true);
-
   cardElement.querySelector('.card__title').textContent = element.name;
   cardElement.querySelector('.card__image').src = element.link;
   cardElement.querySelector('.card__image').alt = element.alt;
-
   cardsList.append(cardElement);
 });
 
-const togglePopup = function (event) {
-  popup.classList.toggle("popup_opened");
-  if (popup.classList.contains("popup_opened")) {
+//popups
+function togglePopup(popupElement) {
+  popupElement.classList.toggle("popup_opened");
+  if (popupElement.classList.contains("popup_opened") && popupElement.classList.contains("popup_type_edit-profile")) {
   nameInput.value = profileName.textContent;
   workInput.value = profileWork.textContent;
   }
 }
 
-// Закрываем и открываем попап кликом на соответсвующие кнопки
-openButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+function closePopup(evt) {
+  let button = evt.target;
+  let popupElement = button.closest(".popup");
+  togglePopup(popupElement);
+}
+
+// Открываем попапы кликом на соответсвующие кнопки
+editProfileButton.addEventListener('click', function() {
+  togglePopup(popupProfileEdit); // открываем попап редактирования
+});
+
+addCardButton.addEventListener('click', function() {
+  togglePopup(popupAddCard); // открываем попап добавления
+});
+
+// Закрываем попапы кликом на крестик
+closeButtons.forEach((button) => {
+  button.addEventListener("click", closePopup);
+});
 
 // Обработчик «отправки» формы
 function formSubmitHandler (evt) {
   evt.preventDefault();  // Отменяем стандартную отправку формы
   profileName.textContent = nameInput.value;
   profileWork.textContent = workInput.value;
-  togglePopup();
+  togglePopup(popupProfileEdit);
 }
 
 // Прикрепляем обработчик к форме
