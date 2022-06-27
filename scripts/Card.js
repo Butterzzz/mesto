c9689f4108e72e93ddddfa263166187f6c69a2ee
Grinchenko Sergei <butters.blaga@gmail.com>
2022-06-28 04:14:46 +0900

    style: add comments

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,5 +1,6 @@
 import { openPopup, popupPhotoView } from './index.js';
 
+// Принимает в конструктор данные карточки и селектор её template-элемента
 export default class Card {
 
   constructor(name, link, cardSelector) {
@@ -8,6 +9,7 @@ export default class Card {
     this._cardSelector = cardSelector;
   }
 
+  // Приватный метод, который находит шаблон элемента карточки, клонирует его и возвращает
   _getTemplate() {
       const cardElement = document
       .querySelector(this._cardSelector)
@@ -18,14 +20,17 @@ export default class Card {
       return cardElement;
   }
 
+  // Приватный метод, который меняет состояние "лайка"
   _handleElementLike() {
     this._element.querySelector('.card__button_action_like').classList.toggle('card__button_active');
   }
 
+  // Приватный метод, который удаляет элемент из DOM
   _handleElementDelete() {
     this._element.remove();
   }
 
+  // Приватный метод, который наполняет карточку и открывает попап с картинкой
   _imagePreview() {
     popupPhotoView.querySelector('.popup__image').src = this._link;
     popupPhotoView.querySelector('.popup__image').alt = this._name;
@@ -33,17 +38,17 @@ export default class Card {
     openPopup(popupPhotoView);
   }
 
-  // Обработчики
+  // Приватный метод, который устанавливает обработчики
   _setEventListeners() {
     this._element.querySelector('.card__button_action_like').addEventListener('click', () => this._handleElementLike());
     this._element.querySelector('.card__button_action_delete').addEventListener('click', () => this._handleElementDelete());
     this._element.querySelector('.card__image').addEventListener('click', () => this._imagePreview());
   }
 
-  // Подготавливаем карточку к публикации
+  // Публичный метод, который возвращает полностью работоспособный и наполненный данными элемент карточки
   generateCard() {
     this._element = this._getTemplate();
-    this._setEventListeners(); // добавим обработчики
+    this._setEventListeners();
 
     this._element.querySelector('.card__title').textContent = this._name;
     this._element.querySelector('.card__image').src = this._link;
@@ -51,5 +56,6 @@ export default class Card {
 
     return this._element;
   }
+
 }
 
