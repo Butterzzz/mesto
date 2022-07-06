242b60e367b6870a775558e7a74766e14f1adeb3
Grinchenko Sergei <butters.blaga@gmail.com>
2022-07-07 02:29:26 +0900

    refactor: add class UserInfo

---
+++ components/UserInfo.js
@@ -0,0 +1,26 @@
+// Класс, который отвечает за управление отображением информации о пользователе на странице
+export default class UserInfo {
+
+  // Принимает в конструктор объект с селекторами двух элементов: элемента имени пользователя и элемента информации о себе
+    constructor({ nameSelector, workselector }) {
+      this._profileName = document.querySelector(nameSelector);
+      this._prifileJob = document.querySelector(workselector);
+    }
+
+  // Публичный метод, который возвращает объект с данными пользователя. Этот метод пригодится когда данные пользователя нужно будет подставить в форму при открытии
+    getUserInfo() {
+      this._userInfoObj = {
+        name: this._profileName.textContent,
+        work: this._profileWork.textContent
+      }
+
+      return this._userInfoObj;
+    }
+
+  // Публичный метод, который принимает новые данные пользователя и добавляет их на страницу
+    setUserInfo(name, work) {
+      this._profileName.textContent = name;
+      this._profileWork.textContent = work;
+    }
+
+  }
