9f386e680eac6ee75db6a085cdecec5a6c636661
Grinchenko Sergei <butters.blaga@gmail.com>
2022-07-08 04:06:41 +0900

    refactor: fix const

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -1,20 +1,22 @@
 import Popup from './Popup.js';
-import { popupPhotoImage, popupPhotoCaption } from '../utils/constants.js';
 
 // Класс, который который наследуется от класса Popup. Класс должен перезаписывать родительский метод open
 export default class PopupWithImage extends Popup {
 
   // Принимает в конструктор единственный параметр — селектор попапа
-    constructor(popupSelector) {
-      super(popupSelector);
-    }
+  constructor(popupSelector) {
+    super(popupSelector);
+
+    this._popupPhotoImage = this._popupElement.querySelector('.popup__image');
+    this._popupPhotoCaption = this._popupElement.querySelector('.popup__image-caption');
+  }
 
   // Публичный метод, который вставляет в попап картинку с src изображения и подписью к картинке
-    open(name, link) {
-      popupPhotoImage.src = link;
-      popupPhotoImage.alt = name;
-      popupPhotoCaption.textContent = name;
-      super.open();
-    }
-
+  open(name, link) {
+    this._popupPhotoImage.src = link;
+    this._popupPhotoImage.alt = name;
+    this._popupPhotoCaption.textContent = name;
+    super.open();
   }
+
+}
