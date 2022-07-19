324faa0b2f291a9a4c0da7b640d2dc19f578d28e
Grinchenko Sergei <butters.blaga@gmail.com>
2022-07-20 02:35:52 +0900

    refactor: add class Api

---
+++ src/components/Api.js
@@ -0,0 +1,27 @@
+// Класс, который описывает запросы к серверу
+export default class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  // Публичный метод, который подгружает начальные карточки с сервера
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'GET',
+      headers: {
+        authorization: this._headers.authorization
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        // если ошибка, отклоняем промис
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+  }
+
+  // другие методы работы с API
+}
