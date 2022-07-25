7bca18f94565074d654050fcf6f03a060cee61bc
Grinchenko Sergei <butters.blaga@gmail.com>
2022-07-26 04:31:26 +0900

    refactor: change some methods

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,19 @@ export default class Card {
 
   // Принимает в конструктор данные карточки, селектор её template-элемента
   // и функцию открытия попапа с картинкой при клике на карточку
-  constructor(name, link, cardSelector, handleCardClick) {
+  constructor(name, link, likes, id, userId, ownerId, cardSelector, { handleCardClick, handleDeleteClick, handleLikeClick }) {
     this._name = name;
     this._link = link;
+    this._likes = likes;
+    this._id = id;
+    this._userId = userId;
+    this._ownerId = ownerId;
+
     this._cardSelector = cardSelector;
 
     this._handleCardClick = handleCardClick;
+    this._handleDeleteClick = handleDeleteClick;
+    this._handleLikeClick = handleLikeClick;
   }
 
   // Приватный метод, который находит шаблон элемента карточки, клонирует его и возвращает
@@ -20,13 +27,39 @@ export default class Card {
       .cloneNode(true);
   }
 
-  // Приватный метод, который меняет состояние "лайка"
-  _handleElementLike() {
-    this._likeButton.classList.toggle('card__button_active');
+  // // Приватный метод, который меняет состояние "лайка"
+  // _handleElementLike() {
+  //   this._likeButton.classList.toggle('card__button_active');
+  // }
+
+  isLiked() {
+    const userHasLikedCard = this._likes.find(
+      user => user._id === this._userId
+    );
+    return userHasLikedCard;
   }
 
-  // Приватный метод, который удаляет элемент из DOM
-  _handleElementDelete() {
+  setLikes(newLikes) {
+    this._likes = newLikes;
+    const likeCountElement = this._element.querySelector('.card__like-counter');
+    likeCountElement.textContent = newLikes.length;
+    if (this.isLiked()) {
+      this._addLike();
+    } else {
+      this._removeLike();
+    }
+  }
+
+  _addLike () {
+    this._likeButton.classList.add('card__button_active');
+  }
+
+  _removeLike () {
+    this._likeButton.classList.remove('card__button_active');
+  }
+
+  // Публичный метод, который удаляет элемент из DOM
+  deleteImage() {
     this._element.remove();
     this._element = null;
   }
@@ -34,10 +67,11 @@ export default class Card {
   // Приватный метод, который устанавливает обработчики
   _setEventListeners() {
     this._likeButton = this._element.querySelector('.card__button_action_like');
+    this._deleteButton = this._element.querySelector('.card__button_action_delete');
     this._cardImage = this._element.querySelector('.card__image');
 
-    this._likeButton.addEventListener('click', () => this._handleElementLike());
-    this._element.querySelector('.card__button_action_delete').addEventListener('click', () => this._handleElementDelete());
+    this._likeButton.addEventListener('click', () => this._handleLikeClick(this._id));
+    this._deleteButton.addEventListener('click', () => this._handleDeleteClick(this._id));
     this._cardImage.addEventListener('click', () => this._handleCardClick(this._name, this._link));
   }
 
@@ -45,6 +79,12 @@ export default class Card {
   generateCard() {
     this._element = this._getTemplate();
     this._setEventListeners();
+
+    this.setLikes(this._likes);
+
+    if (this._ownerId !== this._userId) {
+      this._deleteButton.style.display = 'none';
+    }
 
     this._element.querySelector('.card__title').textContent = this._name;
     this._cardImage.src = this._link;
